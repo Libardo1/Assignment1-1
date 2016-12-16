49e3fdab7f2ff46776e0e8f05be57f5fab51557b
felipessalvatore <felipessalvador@googlemail.com>
2016-12-16 13:11:34 -0200

    Add tree tests

--- q1_softmax.py
+++ q1_softmax.py
@@ -76,7 +76,50 @@ def test_softmax_basic():
     assert np.amax(np.fabs(test3 - np.array(
         [0.73105858, 0.26894142]))) <= 1e-6
 
-    print "You should verify these results!\n"
+    print("You should verify these results!\n")
+
+class TestSoftmax(unittest.TestCase):
+
+    #Here we test if the result of the softmax function
+    #lies in the interval [0,1].
+    #One thing, in some tests we have:
+    #np.sum(softmax(y)) = 1.0000000000000002
+    #I don't know if this is a problem or not. So I change
+    def test_upperbound(self):
+        for k in range(300):
+            y = np.ndarray(shape=(10), dtype=float)
+            for i in range(10):
+                y[i] = random.randint(-100,100)
+            sum_test = np.sum(softmax(y))
+            self.assertTrue(sum_test <=1.0001, \
+                            "\n if y = {0} \n, then np.sum(softmax(y)) = {1} is bigger than 1"\
+                            .format(y,sum_test))
+
+    #Here we test if the softmax function can handle
+    #arrays with high std like [1001,1,1,1]
+    def test_high_std(self):
+        for k in range(300):
+            y = np.ndarray(shape=(10), dtype=float)
+            for i in range(0,9):
+                y[i] = random.randint(1,3)
+            y[9] = random.randint(1001,1100)
+            self.assertFalse(isnan(np.sum(softmax(y))), \
+                            "\n if y = {0} \n, then std = {1} and the softmax will not work"\
+                            .format(y,np.std(y)))
+
+    #Here we test if the softmax function can handle
+    #arrays with big and and low numbers
+    def test_high_low(self):
+        for k in range(300):
+            y = np.ndarray(shape=(2,2), dtype=float)
+            y[0][0] = random.randint(-20000,-10000)
+            y[0][1] = y[0][0]+1
+            y[1][0] = random.randint(10000,20000)
+            y[1][1] = y[1][0]+1
+            self.assertTrue(np.amax(np.fabs(softmax(y) - np.array(
+        [[0.26894142, 0.73105858], [0.26894142, 0.73105858]]))) <= 1e-6, \
+                            "\n if y = {0} \n, then softmax is {1}: problem"\
+                            .format(y,softmax(y)))
 
 def test_softmax():
     """ 
@@ -85,13 +128,13 @@ def test_softmax():
     This function will not be called by the autograder, nor will
     your tests be graded.
     """
-    print "Running your tests..."
+    print("Running your tests...")
     ### YOUR CODE HERE
-    t1 = np.array([1,2,3,222,-100])
-    t1 = np.sum(softmax(t1))
-    print(t1)
-
-
+    suite = unittest.TestSuite()
+    for method in dir(TestSoftmax):
+       if method.startswith("test"):
+          suite.addTest(TestSoftmax(method))
+    unittest.TextTestRunner().run(suite)
     ### END YOUR CODE  
 
 if __name__ == "__main__":
