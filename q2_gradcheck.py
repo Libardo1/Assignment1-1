6cbeb1a1a90ebcbfd21e75e1774afc87ee34dcba
felipessalvatore <felipessalvador@egooglemail.com>
2017-01-25 18:14:49 -0200

    Question 2f solved

--- q2_gradcheck.py
+++ q2_gradcheck.py
@@ -1,42 +1,72 @@
 import numpy as np
 import random
+import unittest
+from q2_sigmoid import sigmoid, sigmoid_grad
 
 # First implement a gradient checker by filling in the following functions
+
+
 def gradcheck_naive(f, x):
-    """ 
-    Gradient check for a function f 
-    - f should be a function that takes a single argument and outputs the cost and its gradients
+    """
+    Gradient check for a function f
+    - f should be a function that takes a single argument
+    and outputs the cost and its gradients
     - x is the point (numpy array) to check the gradient at
-    """ 
+    """
 
     rndstate = random.getstate()
-    random.setstate(rndstate)  
-    fx, grad = f(x) # Evaluate function value at original point
+    random.setstate(rndstate)
+    # Evaluate function value at original point
+    fx, grad = f(x)
     h = 1e-4
 
+    """
+     --- about nd.nditer ---
+    flags = multi_index causes a multi-index, or a tuple of indices
+    with one per iteration dimension, to be tracked.
+    op_flags : list of list of str, optional
+    this is a list of flags for each operand. At minimum, one of readonly,
+    readwrite, or writeonly must be specified.
+    readwrite indicates the operand will be read from and written to.
+    """
+
     # Iterate over all indexes in x
+    all_dif = np.array(x, copy=True)
     it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
     while not it.finished:
         ix = it.multi_index
 
-        ### try modifying x[ix] with h defined above to compute numerical gradients
-        ### make sure you call random.setstate(rndstate) before calling f(x) each time, this will make it 
-        ### possible to test cost functions with built in randomness later
-        ### YOUR CODE HERE:
-        raise NotImplementedError
-        ### END YOUR CODE
+        """ try modifying x[ix] with h defined above to compute numerical gradients
+        make sure you call random.setstate(rndstate) before calling f(x)
+        each time, this will make it possible to test cost functions with
+        built in randomness later"""
+        # YOUR CODE HERE:
+        x_plus_h = np.array(x, copy=True)
+        x_plus_h[ix] = x_plus_h[ix] + h
+        random.setstate(rndstate)
+        fxh_plus, _ = f(x_plus_h)
+        x_minus_h = np.array(x, copy=True)
+        x_minus_h[ix] = x_minus_h[ix] - h
+        random.setstate(rndstate)
+        fxh_minus, _ = f(x_minus_h)
+        numgrad = (fxh_plus - fxh_minus)/(2*h)
+        # END YOUR CODE
 
         # Compare gradients
         reldiff = abs(numgrad - grad[ix]) / max(1, abs(numgrad), abs(grad[ix]))
+        all_dif[ix] = reldiff
         if reldiff > 1e-5:
-            print "Gradient check failed."
-            print "First gradient error found at index %s" % str(ix)
-            print "Your gradient: %f \t Numerical gradient: %f" % (grad[ix], numgrad)
-            return
-    
-        it.iternext() # Step to next dimension
+            print("First gradient error found at index %s" % str(ix))
+            print("Your gradient: %f \t Numerical gradient: %f" % (grad[ix],
+                                                                   numgrad))
+            return reldiff
+        # Step to next dimension
+        it.iternext()
+    print("Gradient check passed!")
 
-    print "Gradient check passed!"
+    # add this return to test
+    return all_dif
+
 
 def sanity_check():
     """
@@ -44,23 +74,41 @@ def sanity_check():
     """
     quad = lambda x: (np.sum(x ** 2), x * 2)
 
-    print "Running sanity checks..."
+    print("Running sanity checks...")
     gradcheck_naive(quad, np.array(123.456))      # scalar test
     gradcheck_naive(quad, np.random.randn(3,))    # 1-D test
-    gradcheck_naive(quad, np.random.randn(4,5))   # 2-D test
-    print ""
+    gradcheck_naive(quad, np.random.randn(4, 5))   # 2-D test
+    print("")
 
-def your_sanity_checks(): 
+
+class TestGrad(unittest.TestCase):
+
+    def test_sigmoid(self):
+        sig = lambda x: (sigmoid(x), sigmoid_grad(sigmoid(x)))
+
+        random_ints = np.random.randint(1, 100, 300)
+        random_floats = np.random.random_sample((300,))
+        random_floats = random_ints*random_floats
+        for number in random_floats:
+            result = gradcheck_naive(sig, np.array(number))
+            self.assertTrue(float(result) <= 1e-5)
+
+
+def your_sanity_checks():
     """
     Use this space add any additional sanity checks by running:
-        python q2_gradcheck.py 
+        python q2_gradcheck.py
     This function will not be called by the autograder, nor will
     your additional tests be graded.
     """
-    print "Running your sanity checks..."
-    ### YOUR CODE HERE
-    raise NotImplementedError
-    ### END YOUR CODE
+    print("Running your sanity checks...")
+    # ## YOUR CODE HERE
+    suite = unittest.TestSuite()
+    for method in dir(TestGrad):
+        if method.startswith("test"):
+            suite.addTest(TestGrad(method))
+    unittest.TextTestRunner().run(suite)
+    # ## END YOUR CODE
 
 if __name__ == "__main__":
     sanity_check()
